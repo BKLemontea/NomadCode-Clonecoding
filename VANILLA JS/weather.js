828f65fe37c86e061c26b9527a28d476f4ed3765
BKLemonTea <jyf1128@naver.com>
2020-01-12 20:15:10 +0900

    Day7

--- VANILLA JS/weather.js
+++ VANILLA JS/weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "01ce0de290ce50a8528673c7cd5a33c5";
 const COORDS = 'coords';
+
+function getWeather(lat,lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){ // then은 데이터가 완전히 들어온 다음 호출함.
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -6,12 +23,13 @@ function saveCoords(coordsObj){
 
 function handleGeoSucces(position){
     const latitude = position.coords.latitude; // 위도
-    const longtitude = position.coords.longtitude; // 경도
+    const longitude = position.coords.longitude; // 경도
     const coordsObj = {
         latitude, // latitude: latitude, 이름이 같은 경우에는 생략 가능 ex) latitude
-        longtitude
+        longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function handleGeoError(){
@@ -27,7 +45,8 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
